ff74beb69253c78bb26782c47bc1de5a3d000e06
Andrey Myaskov <myaskov.andrey@gmail.com>
2023-03-16 15:01:09 +0300

    add inventory

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName ="New item",menuName = "Item/Create New Item")]
+[CreateAssetMenu(fileName ="New item",order =51)]
 public class Item : ScriptableObject
 {
     public int id;
--- Assets/Scripts/Player/InventoryManager.cs
+++ Assets/Scripts/Player/InventoryManager.cs
@@ -5,31 +5,71 @@ using UnityEngine;
 public class InventoryManager : MonoBehaviour
 {
     public static InventoryManager Instance;
-    public List<Item> inventory = new();
+    public Item emptyItem;
+    public List<InventorySlot> inventory = new();
+    [SerializeField] int selectedSlot;
 
     private void Awake()
     {
         Instance = this;
+        selectedSlot = 0;
+        for (int i = 0; i<=5;i++){
+            inventory.Add(new InventorySlot{item = emptyItem, quantity = 0});
+        }
     }
 
     public void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1)){
-            Debug.Log(inventory[0]);
+            selectedSlot = 0;
+            Debug.Log(inventory[selectedSlot].item.name +" amount "+ inventory[selectedSlot].quantity + " in slot "+ selectedSlot);
         }
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            Debug.Log(inventory[1]);
+            selectedSlot = 1;
+            Debug.Log(inventory[selectedSlot].item.name +" amount "+ inventory[selectedSlot].quantity+ " in slot "+ selectedSlot);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            selectedSlot = 2;
+            Debug.Log(inventory[selectedSlot].item.name +" amount "+ inventory[selectedSlot].quantity + " in slot "+ selectedSlot);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            selectedSlot = 3;
+            Debug.Log(inventory[selectedSlot].item.name +" amount "+ inventory[selectedSlot].quantity + " in slot "+ selectedSlot);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            selectedSlot = 4;
+            Debug.Log(inventory[selectedSlot].item.name +" amount "+ inventory[selectedSlot].quantity + " in slot "+ selectedSlot);
         }
     }
 
     public void AddItem(Item item)
     {
-        inventory.Add(item);
+            for(int i =0; i<=inventory.Capacity;i++){
+                if(item.id == inventory[i].item.id && item.stackable){
+                    inventory[i].quantity++;
+                    Debug.Log("increase quantity for item '"+ item + "' in slot:" + i);
+                    
+                    return;
+                }
+                if (inventory[i].item.id == 0){
+                    inventory[i] = new InventorySlot{item=item, quantity = 1};
+                    Debug.Log("new item stored in slot:" + i);
+                    return;
+                }
+        }
     }
 
     public void RemoveItem(Item item)
     {
-        inventory.Remove(item);
+        // inventory.Remove(item);
     }
+}
+
+public class InventorySlot{
+    public Item item;
+    public int quantity;
 }
